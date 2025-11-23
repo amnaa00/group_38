94b9c331f8e32f4e360712955e952aea1a7a7a56
amnaa00 <amnayasser.13@gmail.com>
2025-11-23 19:59:35 +0800

    Delete block_C_interface/block_C_interface.h

--- block_C_interface/block_C_interface.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void initBluetooth();
-void initOLED();
-void handleBluetooth();
-void handleAutoWithObstacle();
-void showStatusOnOLED(const char* msg);
-bool isManualMode();
-int getSelectedDestination();
